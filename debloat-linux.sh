bda8a01413c438c794e8d9bb75788088df5fe546
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-08-26 15:55:35 +0800

    merged debloat linux beta

--- debloat-linux.sh
+++ debloat-linux.sh
@@ -1,88 +1,110 @@
 #!/bin/bash
 
 gapps_list="
-com.google.android.keep                          # Google Keep Notes
-com.google.android.apps.wellbeing                # Digital Wellbeing
-com.google.android.apps.nbu.paisa.user           # Google Pay
-com.google.android.projection.gearhead           # Android Auto
-com.google.android.gm                            # Gmail
-com.google.android.googlequicksearchbox          # Google
-com.google.android.marvin.talkback               # Android Accessibility Suite
-com.android.hotwordenrollment.okgoogle           # OK Google Enrollment
-com.google.android.tts                           # Google Text-To-Speech Engine
-com.google.android.feedback                      # Market Feedback Agent
-com.google.android.gms.location.history          # Google Location History
-com.google.android.inputmethod.latin             # Gboard (Google Keyboard)
-com.google.android.youtube                       # Youtube
-com.google.android.apps.youtube.music            # Youtube-Music
-com.google.android.dialer                        # Google Phone
-com.google.android.contacts                      # Google Contacts
-com.google.android.apps.messaging                # Google Messages
-com.google.android.apps.googleassistant          # Google Assistant
-com.google.android.apps.podcasts                 # Google Podcast
-com.google.android.apps.subscription             # Google One
+com.google.android.keep
+com.google.android.apps.wellbeing
+com.google.android.apps.nbu.paisa.user
+com.google.android.projection.gearhead
+com.google.android.gm
+com.google.android.googlequicksearchbox
+com.google.android.marvin.talkback
+com.android.hotwordenrollment.okgoogle
+com.google.android.tts
+com.google.android.feedback
+com.google.android.gms.location.history
+com.google.android.inputmethod.latin
+com.google.android.youtube
+com.google.android.apps.youtube.music
+com.google.android.dialer
+com.google.android.contacts
+com.google.android.apps.messaging
+com.google.android.apps.googleassistant
+com.google.android.apps.podcasts
+com.google.android.apps.subscription
 "
 
 coloros_bloat="
-# light
-com.heytap.browser                               # Realme Browser 
-com.oppo.music                                   # Music (RUI-1) 
-com.heytap.music                                 # Music (RUI-2) 
-com.coloros.video                                # Video 
-com.coloros.filemanager                          # File Manager 
-com.coloros.compass2                             # Compass 
-com.redteamobile.roaming                         # ORoaming 
-com.realme.securitycheck                         # Security Analysis 
-com.coloros.phonemanager                         # Phone Manager 
-com.heytap.market                                # App Market 
-com.heytap.themestore                            # Theme Store 
-com.finshell.fin                                 # Finshell payment 
-com.heytap.cloud                                 # Heytap Cloud 
-com.realme.movieshot                             # Movie Shot 
-com.coloros.healthcheck                          # Quick check 
-com.coloros.backuprestore                        # Clone Phone 
-com.coloros.backuprestore.remoteservice          # Clone Phone Remote Service
+com.heytap.browser 
+com.oppo.music 
+com.heytap.music 
+com.coloros.video 
+com.coloros.filemanager 
+com.coloros.compass2 
+com.redteamobile.roaming 
+com.realme.securitycheck 
+com.coloros.phonemanager 
+com.heytap.market 
+com.heytap.themestore 
+com.finshell.fin 
+com.heytap.cloud 
+com.realme.movieshot 
+com.coloros.healthcheck 
+com.coloros.backuprestore 
+com.coloros.backuprestore.remoteservice
 
-# leftover
-com.heytap.usercenter                            # My Realme
-com.coloros.gamespace                            # App Enhancement Services
-com.oppo.quicksearchbox                          # Oppo Search
-com.coloros.assistantscreen                      # Smart Assistant
-com.coloros.weather2                             # Weather 
-com.coloros.soundrecorder                        # Recorder
-com.coloros.screenrecorder                       # Screen Recording
-com.coloros.phonemanager                         # Phone Manager
-com.facebook.system                              # Facebook App Installer
-com.facebook.services                            # Facebook Services
-com.facebook.appmanager                          # Facebook App Manager
-com.android.fmradio                              # FM Radio
-com.coloros.focusmode                            # Focus Mode
-com.heytap.pictorial                             # Lockscreen Magazine 
-com.coloros.oshare                               # Realme Share
-com.nearme.atlas                                 # Secure Payment 
-com.coloros.floatassistant                       # Assistive Ball
-com.coloros.securepay                            # Payment Protection
-com.coloros.smartdrive                           # Smart Driving
-com.coloros.sceneservice                         # Smart Services
-com.coloros.ocrscanner                           # OCR Scanner
-com.coloros.smartsidebar                         # Smart Sidebar
-com.heytap.themestore                            # Theme Store
-com.oppo.operationManual                         # User Guide
-com.coloros.phonenoareainquire                   # Number Origin
-com.ted.number                                   # Identification Of Unknown Numbers
-com.oppo.logkit                                  # Feedback Toolkit (RUI-1)
-com.coloros.logkit                               # Feedback (RUI-2)
-com.coloros.childrenspace                        # Kids Space
-com.heytap.habit.analysis                        # Intelligent Analytics System
-com.heytap.openid                                # OpenID
-com.coloros.lockassistant                        # Lock Assistant
-com.coloros.securitypermission                   # Permission Management Of Phone Manager
-com.coloros.healthservice                        # Health Service
-com.coloros.oppomultiapp                         # App Cloner
+
+com.heytap.usercenter
+com.coloros.gamespace
+com.oppo.quicksearchbox
+com.coloros.assistantscreen
+com.coloros.weather2 
+com.coloros.soundrecorder
+com.coloros.screenrecorder
+com.coloros.phonemanager
+com.facebook.system
+com.facebook.services
+com.facebook.appmanager
+com.android.fmradio
+com.coloros.focusmode
+com.heytap.pictorial 
+com.coloros.oshare
+com.nearme.atlas 
+com.coloros.floatassistant
+com.coloros.securepay
+com.coloros.smartdrive
+com.coloros.sceneservice
+com.coloros.ocrscanner
+com.coloros.smartsidebar
+com.heytap.themestore
+com.oppo.operationManual
+com.coloros.phonenoareainquire
+com.ted.number
+com.oppo.logkit
+com.coloros.logkit
+com.coloros.childrenspace
+com.heytap.habit.analysis
+com.heytap.openid
+com.coloros.lockassistant
+com.coloros.securitypermission
+com.coloros.healthservice
+com.coloros.oppomultiapp
 "
 
-counter=0
+sleeptimer=0s
+
+while getopts i,h flag
+do
+case $flag in
+    i)
+        echo "this script is made to automate the process of debloating realme UI 1 / 2"
+        echo "by @Hakimi0804"
+        exit
+        ;;
+    h)
+        echo "simply run the script and then choose the type of operation that you want to do"
+        echo "launching main menu in 3 sec"
+        sleep 3s
+        mainmenu
+        ;;
+    *)  
+        echo "invalid flag provided"
+        exit
+        ;;
+esac
+done
+
 light () {
+    counter=0
     clear
     echo "removing..."
     sleep 1s
@@ -90,9 +112,12 @@ light () {
     do
         adb shell pm uninstall -k --user 0 $app
         ((counter++))
-        if [ $counter -ge 17 ]
+        if [[ $counter -ge 17 ]]
         then
-        break
+            adb shell pm disable-user com.heytap.market     # App Market 
+            adb shell pm disable-user com.heytap.themestore # Theme Store
+            sleep $sleeptimer
+            break
         fi
     done
     clear
@@ -107,8 +132,9 @@ super () {
     do
         adb shell pm uninstall -k --user 0 $app
     done
-    adb shell pm disable-user com.heytap.market     # App Market 
-    adb shell pm disable-user com.heytap.themestore # Theme Store
+    adb shell pm disable-user com.heytap.market
+    adb shell pm disable-user com.heytap.themestore
+    sleep $sleeptimer
     clear
     donedebloat
 }
@@ -119,21 +145,24 @@ gapps () {
     sleep 1s
     for app in $gapps_list
     do
-    adb shell pm uninstall -k --user 0 $app
+        adb shell pm uninstall -k --user 0 $app
     done
+    sleep $sleeptimer
     clear
     donedebloat
 }
 
 rebloat () {
     clear
+    echo "rebloating..."
     sleep 1s
     for app in $coloros_bloat $gapps_list
     do
         adb shell cmd package install-existing --user 0 $app
     done
-    adb shell pm enable com.heytap.market     # App Market 
-    adb shell pm enable com.heytap.themestore # Theme Store 
+    adb shell pm enable com.heytap.market
+    adb shell pm enable com.heytap.themestore 
+    sleep $sleeptimer
     clear
     donedebloat
 }
@@ -144,13 +173,19 @@ checkadb () {
     echo 1 - "return to menu"
     echo 2 - "exit"
     read -p "Type 1, or 2 then press ENTER: " donecheck
-    if [ $donecheck == 1 ]
+    if [[ $donecheck == 1 ]]
     then
         mainmenu
-    elif [ $donecheck == 2 ]
+    elif [[ $donecheck == 2 ]]
     then
         clear
         exit
+    elif [[ $donecheck == "test" ]]
+    then
+        sleeptimer=20
+        echo "Sleep timer set to 20s"
+        sleep 1.5s
+        mainmenu
     else
         echo "$(tput setaf 1; tput bold)Enter a valid input."
         echo "Returning to menu in 3 seconds.$(tput sgr 0)"
@@ -165,13 +200,19 @@ donedebloat () {
     echo "1 - return to menu"
     echo "2 - exit$(tput sgr 0)"
     read -p "Type 1, or 2 then press ENTER: " donedebloatprompt
-    if [ $donedebloatprompt == 1 ]
+    if [[ $donedebloatprompt == 1 ]]
     then
         mainmenu
-    elif [ $donedebloatprompt == 2 ]
+    elif [[ $donedebloatprompt == 2 ]]
     then
         clear
         exit
+    elif [[ $donedebloatprompt == "test" ]]
+    then
+        sleeptimer=20
+        echo "Sleep timer set to 20s"
+        sleep 1.5s
+        mainmenu
     else
         echo "$(tput setaf 1; tput bold)Enter a valid input."
         echo "Returning to menu in 3 seconds.$(tput sgr 0)"
@@ -185,6 +226,19 @@ killadb () {
     echo "Killing"
     adb kill-server
     donedebloat
+}
+
+custom () {
+    clear
+    read -p "Enter package name (type exit to go back to the menu): " customprompt
+    if [[ $customprompt == "exit" ]]
+    then
+        mainmenu
+    else
+        adb shell pm uninstall -k --user 0 $customprompt
+        sleep $sleeptimer
+        custom
+    fi
 }
 
 mainmenu () {
@@ -204,33 +258,44 @@ mainmenu () {
     echo "4 - rebloat all debloated apps"
     echo "5 - check if adb is working"
     echo "6 - kill adb daemon"
-    echo "7 - exit$(tput sgr 0)"
+    echo "7 - custom package"
+    echo "8 - exit$(tput sgr 0)"
     
-    read -p "Type 1, 2, 3, 4, 5, 6 or 7 then press ENTER: " mode
-    if [ $mode == 1 ]
+    read -p "Type 1, 2, 3, 4, 5, 6, 7 or 8 then press ENTER: " mode
+    if [[ $mode == 1 ]]
     then
         light
-    elif [ $mode == 2 ]
+    elif [[ $mode == 2 ]]
     then
         super
-    elif [ $mode == 3 ]
+    elif [[ $mode == 3 ]]
     then
         gapps
-    elif [ $mode == 4 ]
+    elif [[ $mode == 4 ]]
     then
         rebloat
-    elif [ $mode == 5 ]
+    elif [[ $mode == 5 ]]
     then
         checkadb
-    elif [ $mode == 6 ]
+    elif [[ $mode == 6 ]]
     then
         killadb
-    elif [ $mode == 7 ]
+    elif [[ $mode == 7 ]]
+    then
+        clear
+        custom
+    elif [[ $mode == 8 ]]
     then
         clear
         exit
+    elif [[ $mode == "test" ]]
+    then
+        sleeptimer=20
+        echo "Sleep timer set to 20s"
+        sleep 1.5s
+        mainmenu
     else
-        echo "$(tput setaf 1)Enter a valid input."
+        echo "$(tput setaf 1; tput bold)Enter a valid input."
         echo "Returning to menu in 3 seconds.$(tput sgr 0)"
         sleep 3s
         mainmenu
